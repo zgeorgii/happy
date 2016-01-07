25c8ee35dffc066fd4b5b3a9bdc74aa92cd34fee
Rick Stoner <stonerrick19@gmail.com>
2016-01-07 13:28:34 -0800

    updated readme file stating work I contributed

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
 var counter = 0;
 var res1 = document.getElementById('result1');
-var res2 = document.getElementById('result2')
-var res3 = document.getElementById('result3')
-var res4 = document.getElementById('result4')
+var res2 = document.getElementById('result2');
+var res3 = document.getElementById('result3');
+var res4 = document.getElementById('result4');
 var user = prompt("What is your name?");
  alert("Hi there "+ user + " Imma axe you a question!!!");
   console.log("the user's name is " + user);
   function questionOne(){
     var answer1 = prompt(user + " These are yes/no questions about Kyle Aubin, so please answer with Y or N. Does Kyle like cats?");
     if(answer1.toLowerCase() === "y" || answer1.toLowerCase() === "yes") {
-      res1.textContent = "You're right!!!";
+      res1.textContent = 'You answered: ' + answer1 + ". You're right!!!";
       counter +=1;
     } else {
-      res1.textContent = "You lose :( " + "Kyle like cats.";
+      res1.textContent = 'You answered: ' + answer1 + ". You lose :( " + "Kyle like cats.";
     }
     console.log(answer1);
   }
   function questionTwo(){
     var answer2 = prompt(user + " does Kyle enjoy learning?");
     if(answer2.toLowerCase() === "y" || answer2.toLowerCase() === "yes"){
-      res2.textContent = "YAYYY you're right Kyle enjoy learning!!";
+      res2.textContent = 'You answered: ' + answer2 + ". YAYYY you're right Kyle enjoy learning!!";
       counter +=1;
     } else {
-      res2.textContent = "Awwwh! WRONG!!!";
+      res2.textContent = 'You answered: ' + answer2 + ". Awwwh! WRONG!!!";
     }
     console.log(answer2);
   }
   function questionThree(){
     var answer3 = prompt(user + " are Kyles eyes blue? ");
     if(answer3.toLowerCase() === "y" || answer3.toLowerCase() === "yes") {
-      res3.textContent = user + " you are  correct!";
+      res3.textContent = 'You answered: ' + answer3 + '. ' + user + " you are  correct!";
       counter +=1;
     } else {
-      res3.textContent = "sorry "+ user + " , Kyles eyes are blue.";
+      res3.textContent = 'You answered: ' + answer3 + ". Sorry, "+ user + " , Kyles eyes are blue.";
     }
     console.log(answer3);
   }
@@ -41,7 +41,7 @@ var user = prompt("What is your name?");
       var bonus = prompt(user + " I'm thinking of a number between 1-10. Can you guess what it is?");
       if (bonus == "6") {
         counter +=1;
-        res4.textContent = ("Wow!!!! you're right!!!, GOOD JOB!!! " + user + " you got " + counter +"/4 correct");
+        res4.textContent = 'You answered: ' + bonus + ". Wow!!!! you're right!!!, GOOD JOB!!! " + user + " you got " + counter +"/4 correct";
       } else if(bonus < 6) {
         alert(bonus + " is too low!!");
       } else { alert(bonus + " is too high!!");}
