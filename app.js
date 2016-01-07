66fc486a5d71ae089b500d855e72557f9dbb8418
Rick Stoner <stonerrick19@gmail.com>
2016-01-07 13:04:50 -0800

    Changes alerts to display as text on html page with id tags

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
 var counter = 0;
+var res1 = document.getElementById('result1');
+var res2 = document.getElementById('result2')
+var res3 = document.getElementById('result3')
+var res4 = document.getElementById('result4')
 var user = prompt("What is your name?");
  alert("Hi there "+ user + " Imma axe you a question!!!");
   console.log("the user's name is " + user);
   function questionOne(){
     var answer1 = prompt(user + " These are yes/no questions about Kyle Aubin, so please answer with Y or N. Does Kyle like cats?");
     if(answer1.toLowerCase() === "y" || answer1.toLowerCase() === "yes") {
-      alert("You're right!!!"); counter +=1;
+      res1.textContent = "You're right!!!";
+      counter +=1;
     } else {
-      alert("You lose :( " + "Kyle like cats.");
+      res1.textContent = "You lose :( " + "Kyle like cats.";
     }
     console.log(answer1);
   }
   function questionTwo(){
     var answer2 = prompt(user + " does Kyle enjoy learning?");
     if(answer2.toLowerCase() === "y" || answer2.toLowerCase() === "yes"){
-      alert("YAYYY you're right Kyle enjoy learning!!"); counter +=1;
+      res2.textContent = "YAYYY you're right Kyle enjoy learning!!";
+      counter +=1;
     } else {
-      alert("Awwwh! WRONG!!!");
+      res2.textContent = "Awwwh! WRONG!!!";
     }
     console.log(answer2);
   }
   function questionThree(){
     var answer3 = prompt(user + " are Kyles eyes blue? ");
     if(answer3.toLowerCase() === "y" || answer3.toLowerCase() === "yes") {
-      alert(user + " you are  correct!"); counter +=1;
+      res3.textContent = user + " you are  correct!";
+      counter +=1;
     } else {
-      alert("sorry "+ user + " , Kyles eyes are blue.");
+      res3.textContent = "sorry "+ user + " , Kyles eyes are blue.";
     }
     console.log(answer3);
   }
   function questionFour(){
-    do {var bonus = prompt(user + " I'm thinking of a number between 1-10. Can you guess what it is?");
-    if (bonus == "6") {
-      alert("Wow!!!! you're right!!!"); counter +=1;
-    } else if(bonus < 6) {
-      alert(bonus + " is too low!!");
-    } else { alert(bonus + " is too high!!");}}
-    while (bonus != 6);
-    alert("GOOD JOB!!! "+ user + " you got "+ counter +"/4 correct" );
+    do {
+      var bonus = prompt(user + " I'm thinking of a number between 1-10. Can you guess what it is?");
+      if (bonus == "6") {
+        counter +=1;
+        res4.textContent = ("Wow!!!! you're right!!!, GOOD JOB!!! " + user + " you got " + counter +"/4 correct");
+      } else if(bonus < 6) {
+        alert(bonus + " is too low!!");
+      } else { alert(bonus + " is too high!!");}
+    } while (bonus != 6);
   }
 
   questionOne();
